70ba1b872765e8606e364d186e27c5ae3fccfd0e
68219090008-Champ <68219090008@technictrang.ac.th>
2026-06-19 14:57:01 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledPin = LED_BUILTIN;
+const unsigned long blinkDelay = 1000;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(ledPin, HIGH);
+  delay(blinkDelay);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(ledPin, LOW);
+  delay(blinkDelay);
 }
