9e92962e933c6bf8e4d24af37b956cecaf9ff8b9
68219090008-Champ <68219090008@technictrang.ac.th>
2026-06-19 16:36:17 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-const int ledPin = LED_BUILTIN;
-const unsigned long blinkDelay = 1000;
+const uint8_t DHT_PIN = 4;
+const uint8_t DHT_TYPE = DHT11;
+const unsigned long READ_INTERVAL_MS = 2000;
+
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  dht.begin();
+
+  Serial.println("DHT11 temperature and humidity reader");
+  Serial.println("Data pin: Arduino UNO D4");
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(blinkDelay);
+  const float humidity = dht.readHumidity();
+  const float temperatureC = dht.readTemperature();
 
-  digitalWrite(ledPin, LOW);
-  delay(blinkDelay);
+  if (isnan(humidity) || isnan(temperatureC)) {
+    Serial.println("Failed to read from DHT11 sensor");
+  } else {
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.print(" C  Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+  }
+
+  delay(READ_INTERVAL_MS);
 }
