9f8755e518691a3f509a3bccac33180a97c2f5d4
68219090008-Champ <68219090008@technictrang.ac.th>
2026-06-19 15:34:31 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 
-const int ledPin = LED_BUILTIN;
+const int ledPin1 = 13;
+const int ledPin2 = 12;
 const unsigned long blinkDelay = 1000;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, LOW);
   delay(blinkDelay);
 
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, HIGH);
   delay(blinkDelay);
 }
