46205d9d7477ebe1d240d5ff70ede02b15c63181
68219090008-Champ <68219090008@technictrang.ac.th>
2026-06-24 15:03:22 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,38 @@
 #include <Arduino.h>
 
+const int digitalPin = 3;
+const int analogPin = A0;
 const int ledPin = LED_BUILTIN;
-const unsigned long blinkDelay = 1000;
+const unsigned long readInterval = 300;
 
 void setup() {
+  pinMode(digitalPin, INPUT);
   pinMode(ledPin, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("KY-025 Sensor Test");
+  Serial.println("D0 -> Arduino D3, A0 -> Arduino A0");
+  Serial.println("Move a magnet near the sensor.");
+  Serial.println("--------------------------------");
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(blinkDelay);
+  int digitalValue = digitalRead(digitalPin);
+  int analogValue = analogRead(analogPin);
 
-  digitalWrite(ledPin, LOW);
-  delay(blinkDelay);
+  digitalWrite(ledPin, digitalValue);
+
+  Serial.print("Digital = ");
+  Serial.print(digitalValue);
+  Serial.print(" | Analog = ");
+  Serial.print(analogValue);
+  Serial.print(" | Status = ");
+
+  if (digitalValue == HIGH) {
+    Serial.println("Magnet detected");
+  } else {
+    Serial.println("No magnet");
+  }
+
+  delay(readInterval);
 }
