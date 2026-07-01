04a72a62d8e8d8f327a7b9520588f32ba806cad9
68219090008-Champ <68219090008@technictrang.ac.th>
2026-07-01 15:20:00 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,49 @@
 #include <Arduino.h>
-
-const int ledPin = LED_BUILTIN;
-const unsigned long blinkDelay = 1000;
+#include <Wire.h>
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) {
+  }
+
+  Wire.begin();
+  Serial.println("I2C Scanner");
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(blinkDelay);
+  byte error;
+  byte address;
+  int nDevices = 0;
 
-  digitalWrite(ledPin, LOW);
-  delay(blinkDelay);
+  Serial.println("Scanning...");
+
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.print(address, HEX);
+      Serial.println(" !");
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
+    }
+  }
+
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found");
+  } else {
+    Serial.println("Done");
+  }
+
+  delay(5000);
 }
+
